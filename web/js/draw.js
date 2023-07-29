c3224c738b37e9a50cd029871378c5e77af0c4bb
Teboho Manyeli <temanye022@student.wethinkcode.co.za>
2023-07-29 22:40:39 +0200

    Developed an interactive drawing application. The application supports various inputs including mouse movements and touch screen gestures. An 'undo' button has been implemented to allow users to easily erase their last drawing action. The application's interface is designed using HTML and enhanced with CSS for a better user experience. The application logic is encapsulated within two JavaScript classes. The 'SketchPad' class serves as the main drawing area, handling user inputs and managing the drawing state. The 'drawingFunctions' object contains the actual drawing procedures, enabling the rendering of paths on the canvas. Both classes have been carefully documented with comments and docstrings for better maintainability.

---
+++ web/js/draw.js
@@ -0,0 +1,46 @@
+/**
+ * An object that encapsulates drawing functionality.
+ */
+const drawingFunctions = {};
+
+/**
+ * Draws a path on a given context.
+ * @param {CanvasRenderingContext2D} context - The context where the path will be drawn.
+ * @param {Array} path - An array of points representing the path.
+ * @param {string} color - The color of the path. Default is black.
+ */
+drawingFunctions.path = (context, path, color = "black") => {
+    // Set stroke style
+    context.strokeStyle = color;
+    context.lineWidth = 3;
+
+    // Start the path
+    context.beginPath();
+    context.moveTo(...path[0]);
+
+    // Draw the path
+    for (let pointIndex = 1; pointIndex < path.length; pointIndex++) {
+        context.lineTo(...path[pointIndex]);
+    }
+
+    // Set path properties
+    context.lineCap = "round";
+    context.lineJoin = "round";
+
+    // Render the path
+    context.stroke();
+};
+
+
+/**
+ * Draws multiple paths on a given context.
+ * @param {CanvasRenderingContext2D} context - The context where the paths will be drawn.
+ * @param {Array} paths - An array of path arrays.
+ * @param {string} color - The color of the paths. Default is black.
+ */
+drawingFunctions.paths = (context, paths, color = "black") => {
+    // Draw each path in the array of paths
+    for (const path of paths) {
+        drawingFunctions.path(context, path, color);
+    }
+};
---
+++ web/js/sketchPad.js
@@ -0,0 +1,118 @@
+/**
+ * Class representing a SketchPad.
+ */
+class SketchPad {
+    /**
+     * Create a SketchPad.
+     * @param {Element} container - The HTML element where the SketchPad will be appended.
+     * @param {number} dimension - The width and height of the SketchPad in pixels. Default is 400.
+     */
+    constructor(container, dimension = 400) {
+        // Create a canvas element and set its properties
+        this.canvasElement = document.createElement("canvas");
+        this.canvasElement.width = dimension;
+        this.canvasElement.height = dimension;
+        this.canvasElement.style = `
+            background-color: white;
+            box-shadow: 0px 0px 10px 2px black;
+        `;
+        
+        // Append the canvas to the container
+        container.appendChild(this.canvasElement);
+
+        // Create a line break and append it to the container
+        const lineBreak = document.createElement("br");
+        container.appendChild(lineBreak);
+
+        // Create an undo button, set its properties and append it to the container
+        this.undoButtonElement = document.createElement("button");
+        this.undoButtonElement.innerHTML = "UNDO";
+        container.appendChild(this.undoButtonElement);
+
+        // Set up the context for drawing
+        this.context = this.canvasElement.getContext("2d");
+
+        // Initialize drawing variables
+        this.pathsList = [];
+        this.isDrawing = false;
+
+        // Redraw the canvas
+        this._redraw();
+
+        // Add event listeners to the canvas and undo button
+        this._addEventListeners();
+    }
+
+    /**
+     * Add event listeners to the SketchPad elements.
+     * @private
+     */
+    _addEventListeners() {
+        // Add mouse event listeners to the canvas
+        this.canvasElement.onmousedown = (event) => {
+            const mousePosition = this._getMousePosition(event);
+            this.pathsList.push([mousePosition]);
+            this.isDrawing = true;
+        };
+        this.canvasElement.onmousemove = (event) => {
+            if (this.isDrawing) {
+                const mousePosition = this._getMousePosition(event);
+                const lastPath = this.pathsList[this.pathsList.length - 1];
+                lastPath.push(mousePosition);
+                this._redraw();
+            }
+        };
+        this.canvasElement.onmouseup = () => {
+            this.isDrawing = false;
+        };
+
+        
+        // Add touch event listeners to the canvas
+        this.canvasElement.ontouchstart = (event) => {
+            const touchLocation = event.touches[0];
+            this.canvasElement.onmousedown(touchLocation);
+        };
+        this.canvasElement.ontouchmove = (event) => {
+            const touchLocation = event.touches[0];
+            this.canvasElement.onmousemove(touchLocation);
+        };
+        this.canvasElement.ontouchend = () => {
+            this.canvasElement.onmouseup();
+        };
+
+
+        // Add click event listener to the undo button
+        this.undoButtonElement.onclick = () => {
+            this.pathsList.pop(); // Remove last path
+            this._redraw();
+        };
+    }
+
+    /**
+     * Get mouse position relative to the SketchPad.
+     * @param {MouseEvent} event - The mouse event.
+     * @return {Array} The x and y coordinates of the mouse relative to the SketchPad.
+     * @private
+     */
+    _getMousePosition = (event) => {
+        const rect = this.canvasElement.getBoundingClientRect();
+        return [
+            event.clientX - rect.left,
+            event.clientY - rect.top
+        ];
+    };
+
+    /**
+     * Redraw the SketchPad.
+     * @private
+     */
+    _redraw() {
+        // Clear the canvas
+        this.context.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+        // Draw the paths
+        drawingFunctions.paths(this.context, this.pathsList);
+
+        // Enable or disable the undo button
+        this.undoButtonElement.disabled = this.pathsList.length === 0;
+    }
+}
